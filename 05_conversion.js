f9ea7098c25ba0eaf51dcd3e12ce5043dd3b5061
Deepak Kumar <deepakchaudhary.cse2001@gmail.com>
2024-10-07 15:31:40 +0530

    add conversion file.

---
+++ 05_conversion.js
@@ -0,0 +1,30 @@
+//Checking the type of datatypes.
+
+let g = 9.8;
+console.log(typeof(g));
+
+let str = "Deepak";
+console.log(typeof(str));
+
+let isTrue = true;
+console.log(typeof(isTrue));
+
+let isFalse = false;
+console.log(typeof(isFalse)); 
+
+let javascript;
+console.log(typeof(javascript));
+
+let number = 999;
+let string = String(number);
+console.log(string);
+console.log(typeof(string));
+
+let isBoolean = 1;
+let isBooleanValue = Boolean(isBoolean)
+console.log(isBooleanValue);
+
+
+// "33" => 33
+// "33abc" => Nan 
+// true => 1; false => 0;
