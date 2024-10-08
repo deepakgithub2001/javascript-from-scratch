ec281a9cb29c3e0a540259ab2b79ae32e78ac848
Deepak Kumar <deepakchaudhary.cse2001@gmail.com>
2024-10-08 11:30:25 +0530

    add datatypes-summary file.

---
+++ 07_datatypes-summary.js
@@ -0,0 +1,20 @@
+// Primitive Datatypes:-
+// Number, String, Null, BigInt, Undefined, Symbol, Boolean
+
+//Non-Primitive or Reference Datatypes:-
+// Array, Objects, Functions 
+
+// Return type of variables in JavaScript
+// 1) Primitive Datatypes
+//        Number => number
+//        String  => string
+//        Boolean  => boolean
+//        null  => object
+//        undefined  =>  undefined
+//        Symbol  =>  symbol
+//        BigInt  =>  bigint
+
+// 2) Non-primitive Datatypes
+//        Arrays  =>  object
+//        Function  =>  function
+//        Object  =>  object
