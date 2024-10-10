78cd83bcfb9bf843df386e40f8c244a67618d3a3
Deepak Kumar <deepakchaudhary.cse2001@gmail.com>
2024-10-10 15:00:35 +0530

    add number and math file.

---
+++ 10_nums_and_maths.js
@@ -0,0 +1,32 @@
+// Numbers and Math used mostly in JavaSript. 
+
+//Numbers
+
+// let num = 3; =>3
+// let float =  3.7; =>3.7
+// let sum = 999 + 999; => 1998
+// let diff = 999 - 999; =>0
+// let product = 999 * 999; =>998001
+// let quotient = 10 / 5; =>2
+// let remainder = 9 % 3; =>0
+// let power =  3 ** 3; =>9
+// let number = 10; 
+// console.log(number++); =>10
+// let number = 10; 
+// console.log(++number); =>11
+// let number1 = 10;
+// console.log(number1--); =>10
+// let number2 = 10; =>
+// console.log(--number2); =>9
+
+// Math
+
+// Math.floor(4.3); =>4
+// Math.ceil(4.3); =>5
+// Math.round(4.3); =>4
+// Math.random(); => 0.000001
+// Math.min(1,2,3); =>1
+// Math.max(5,7,9); =>9
+// Math.sqrt(25); =>5
+// Math.pow(2, 3); =>8
+// let result = (1.0 + 2.0).toFixed(3); =>3.00 
