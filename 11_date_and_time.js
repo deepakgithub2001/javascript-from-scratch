96777ca4d832ebbf9c2b9c7d563089e22f84cf83
Deepak Kumar <deepakchaudhary.cse2001@gmail.com>
2024-10-10 16:17:44 +0530

    add date and time file.

---
+++ 11_date_and_time.js
@@ -0,0 +1,25 @@
+// let myDate = new Date();
+// console.log(myDate);
+// =>2024-10-10T10:27:49.452Z
+
+// let myDate = new Date();
+// console.log(myDate.toString());
+// =>Thu Oct 10 2024 15:58:54 GMT+0530 (IST)
+
+
+// let myDate = new Date();
+// console.log(myDate.toDateString());
+// =>Thu Oct 10 2024
+
+
+// let myDate = new Date();
+// console.log(myDate.toISOString());
+// =>2024-10-10T10:30:45.978Z
+
+// let myDate = new Date();
+// console.log(myDate.toJSON());
+// =>2024-10-10T10:31:34.891Z
+
+// let myDate = new Date();
+// console.log(myDate.getDate());
+// =>10
