80a52f66726f71981c3987529807b1862e0cd8f5
Deepak Kumar <deepakchaudhary.cse2001@gmail.com>
2024-10-16 11:41:49 +0530

    add functions2 file.

---
+++ 03_basics/02_functions2.js
@@ -0,0 +1,27 @@
+function priceCalculate(...num1){
+return num1
+} 
+
+// console.log(priceCalculate(5, 7, 9)); // rest operator using ...num1, spread and rest operator are same but the use case of these operators are different.
+
+const user = {
+    name: "Deepak",
+    price: 999 
+} 
+
+function handleObject(anyobject){
+console.log(`Username is ${anyobject.name} and Price is ${anyobject.price}`);
+
+} 
+
+handleObject(user);
+handleObject({
+    name: "Sam",
+    price: 99999
+}) 
+
+const array = [100, 200, 300, 400, 500]
+function firstValue(get_array){
+    return get_array[0]
+}
+console.log(firstValue(array)); 
