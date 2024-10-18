649aad5f43b8841146e236c03bb09fd1c1cf885f
Deepak Kumar <deepakchaudhary.cse2001@gmail.com>
2024-10-18 14:24:14 +0530

    add loop file.

---
+++ 05_iterations/01_loop.js
@@ -0,0 +1,10 @@
+// for loop.
+for (let index = 0; index <= 10; index++) {
+    const element = index;
+    console.log(element);
+} 
+
+for (let index = 0; index <= 10; index++) {
+    const element = index;
+    console.log(element);
+} 
