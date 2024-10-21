4fda2cde8cf422118df3d9e87f27d0fbfc5a31c7
Deepak Kumar <deepakchaudhary.cse2001@gmail.com>
2024-10-21 11:08:47 +0530

    update loop file.

--- 05_iterations/01_loop.js
+++ 05_iterations/01_loop.js
@@ -1,10 +1,37 @@
-// for loop.
+for loop.
 for (let index = 0; index <= 10; index++) {
     const element = index;
     console.log(element);
-} 
+}
 
-for (let index = 0; index <= 10; index++) {
-    const element = index;
-    console.log(element);
-} 
+for (let i = 1; i <= 10; i++) {
+    console.log(`The Outer Value ${i}`);
+    for (let j = 1; j <= 10; j++) {
+       console.log(i + "*" + j + "=" + i * j);
+       }
+    }  
+
+    let superhero = ["flash", "bataman", "superman"]
+    for (let index = 0; index < superhero.length; index++) {
+        const element = superhero[index];
+        console.log(element);
+        
+    }
+
+    for (let index = 1; index <= 10; index++ ){
+        if (index == 5) {
+            console.log("detected 5");
+            // break;
+        }
+        console.log(`The value is ${index}`);
+        
+    }
+
+    for (let index = 1; index <= 10; index++ ){
+        if (index == 5) {
+            console.log("detected 5");
+            continue;
+        }
+        console.log(`The value is ${index}`);
+        
+    } 
