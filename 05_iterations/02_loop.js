63f5a82aec3f73a20000375cd589ba6bf67f1ee3
Deepak Kumar <deepakchaudhary.cse2001@gmail.com>
2024-10-21 12:06:53 +0530

    add loop file.

---
+++ 05_iterations/02_loop.js
@@ -0,0 +1,23 @@
+// while and do while loop.
+
+// let index = 1;
+
+// while (index <= 10) {
+//     console.log(`The Value is ${index}`);
+//     index = index + 1;
+// } 
+
+
+// let superman = ["flash", "batman", "superman"]
+// let array = 0;
+// while (array < superman.length) {
+//  console.log(`The value is ${superman[array]}`);
+//  array = array + 1;
+     
+// }
+
+let score = 1;
+do {
+    console.log(`The score is ${score} `);
+    score = score + 1;
+} while (score <= 10) 
