a1470ec7deaa4d264421997acc495b331c8a54eb
Deepak Kumar <deepakchaudhary.cse2001@gmail.com>
2024-10-21 14:12:09 +0530

    update loop file.

--- 05_iterations/01_loop.js
+++ 05_iterations/01_loop.js
@@ -21,7 +21,7 @@ for (let i = 1; i <= 10; i++) {
     for (let index = 1; index <= 10; index++ ){
         if (index == 5) {
             console.log("detected 5");
-            // break;
+            break;
         }
         console.log(`The value is ${index}`);
         
