efaaff25a7f7b6c0769c5c73e95d0a86bcea3d56
Deepak Kumar <deepakchaudhary.cse2001@gmail.com>
2024-10-21 15:16:50 +0530

    add loop file.

---
+++ 05_iterations/03_loop.js
@@ -0,0 +1,25 @@
+// The High-Order Arrays loops
+
+//  for of loop.
+
+const array = [1,2,3,4,5,6,7,8,9,10]
+
+for (const num of array) {
+    console.log(num);
+
+}
+
+
+const greetings = "Hello World!"
+for (const greet of greetings) {
+    console.log(`each character is greet ${greet}`);
+
+}
+Map
+
+const map = new Map
+map.set("IN", "India")
+map.set("USA", "United States of America") 
+map.set("FR", "France")  
+
+console.log(map); 
