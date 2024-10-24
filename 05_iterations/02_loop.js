0b99662fbf92b2d139da6053e6cd0caaa67a4f81
Deepak Kumar <deepakchaudhary.cse2001@gmail.com>
2024-10-24 11:38:03 +0530

    update loop file.

--- 05_iterations/02_loop.js
+++ 05_iterations/02_loop.js
@@ -5,15 +5,15 @@ let index = 1;
 while (index <= 10) {
     console.log(`The Value is ${index}`);
     index = index + 1;
-} 
+}
 
 
 let superman = ["flash", "batman", "superman"]
 let array = 0;
 while (array < superman.length) {
- console.log(`The value is ${superman[array]}`);
- array = array + 1;
-     
+    console.log(`The value is ${superman[array]}`);
+    array = array + 1;
+
 }
 
 let score = 1;
