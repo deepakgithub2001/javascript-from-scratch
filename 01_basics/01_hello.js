71e55a0d020c98a1ea9ee6cefb9cea14ca613229
Deepak Kumar <deepakchaudhary.cse2001@gmail.com>
2025-01-03 17:45:07 +0530

    update hello file.

--- 01_basics/01_hello.js
+++ 01_basics/01_hello.js
@@ -1,7 +1,5 @@
-console.log("Hello World!");
-const arr = ([1] == String)
-console.log(arr)
-
-
+console.log("Hello World!"); //Hello World!
+const arr = ([1] == String) 
+console.log(arr); //false
 const arr1 = ([] == []);
-console.log(arr1);
+console.log(arr1); //false
