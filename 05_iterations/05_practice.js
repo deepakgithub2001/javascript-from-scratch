3ee00be7c8f679f23d569d48a30b3938dd47922c
Deepak Kumar <deepakchaudhary.cse2001@gmail.com>
2025-01-15 16:25:12 +0530

    add practice file.

---
+++ 05_iterations/05_practice.js
@@ -0,0 +1,36 @@
+// Write a loop to print numbers from 1 to 10.
+for (let i = 1; i <= 10; i++) {
+    console.log(i);
+}
+
+// Write a program to print all even numbers between 1 and 20.
+let j = 1;
+while (j <= 20) {
+    if (j % 2 == 0) {
+        console.log(i);
+    }
+    j++;
+}
+
+// Use a for loop to calculate the sum of numbers from 1 to 100.
+
+let sum = 0;
+for (let j = 1; j <= 100; j++) {
+    sum = sum + j;
+}
+console.log("The Sum of 1 to 100 is", sum);
+
+// Write a program to print the elements of an array.
+fruits = ["Apple", "Banana", "Orange"]
+for (let i = 0; i < fruits.length; i++) {
+    console.log(fruits[i]);
+}
+
+// Use a do...while loop to print numbers from 10 down to 1.
+
+let i = 10
+do {
+    console.log(i);
+    i--;
+
+} while (i >= 1);
