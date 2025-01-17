9f21d609809e14fa1d07c56ce72a2045a2ece82b
Deepak Kumar <deepakchaudhary.cse2001@gmail.com>
2025-01-17 10:29:20 +0530

    update practice file.

--- 05_iterations/05_practice.js
+++ 05_iterations/05_practice.js
@@ -1,36 +1,63 @@
-// Write a loop to print numbers from 1 to 10.
-for (let i = 1; i <= 10; i++) {
-    console.log(i);
+// // Write a loop to print numbers from 1 to 10.
+// for (let i = 1; i <= 10; i++) {
+//     console.log(i);
+// }
+
+// // Write a program to print all even numbers between 1 and 20.
+// let j = 1;
+// while (j <= 20) {
+//     if (j % 2 == 0) {
+//         console.log(i);
+//     }
+//     j++;
+// }
+
+// // Use a for loop to calculate the sum of numbers from 1 to 100.
+
+// let sum = 0;
+// for (let j = 1; j <= 100; j++) {
+//     sum = sum + j;
+// }
+// console.log("The Sum of 1 to 100 is", sum);
+
+// // Write a program to print the elements of an array.
+// fruits = ["Apple", "Banana", "Orange"]
+// for (let i = 0; i < fruits.length; i++) {
+//     console.log(fruits[i]);
+// }
+
+// // Use a do...while loop to print numbers from 10 down to 1.
+
+// let i = 10
+// do {
+//     console.log(i);
+//     i--;
+
+// } while (i >= 1);
+
+
+// Write a program to find the factorial of a number using a loop.
+// let number = 5;
+// let factorial = 1
+
+// for (let i = 1; i <= number; i++) {
+//     factorial = factorial * i;
+//     console.log(factorial);
+// }
+
+// console.log(factorial);
+
+// Write a program to reverse a given string using a loop.
+let str = "generate"
+let reversedString = "";
+for (let i = str.length - 1; i >= 0; i--) {
+    reversedString = reversedString + str[i];
 }
 
-// Write a program to print all even numbers between 1 and 20.
-let j = 1;
-while (j <= 20) {
-    if (j % 2 == 0) {
-        console.log(i);
-    }
-    j++;
-}
+console.log("The Original String is:", str);
+console.log("The Reversed String is:", reversedString);
 
-// Use a for loop to calculate the sum of numbers from 1 to 100.
 
-let sum = 0;
-for (let j = 1; j <= 100; j++) {
-    sum = sum + j;
-}
-console.log("The Sum of 1 to 100 is", sum);
-
-// Write a program to print the elements of an array.
-fruits = ["Apple", "Banana", "Orange"]
-for (let i = 0; i < fruits.length; i++) {
-    console.log(fruits[i]);
-}
-
-// Use a do...while loop to print numbers from 10 down to 1.
-
-let i = 10
-do {
-    console.log(i);
-    i--;
-
-} while (i >= 1);
+// Write a program to iterate through an object and print its keys and values.
+// Use a nested loop to print a 5x5 grid of stars (*).
+// Write a loop to check for a specific value in an array. If the value exists, break the loop and print a message.
